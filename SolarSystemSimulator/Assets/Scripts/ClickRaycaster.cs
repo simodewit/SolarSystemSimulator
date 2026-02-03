0c9df09f8b77db0b1474c851a03c3f6d47eac0d5
Atmojomojo <rotastisch@gmail.com>
2026-02-03 11:00:07 +0100

    Import TMPro
    also add canvas to galaxyscene

--- SolarSystemSimulator/Assets/Scripts/ClickRaycaster.cs
+++ SolarSystemSimulator/Assets/Scripts/ClickRaycaster.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClickRaycaster : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Camera _cam;
+
+    private void Awake()
     {
-        
+        _cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick(InputAction.CallbackContext ctx)
     {
-        
+        if (!ctx.performed) return;
+
+        Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            hit.collider.SendMessage("OnClicked", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
--- SolarSystemSimulator/Assets/Scripts/PlanetManager.cs
+++ SolarSystemSimulator/Assets/Scripts/PlanetManager.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
-    
+    public void LockToPlanet()
+    {
+
+    }
+
+    private void LetPlanetGo()
+    {
+
+    }
+
+    private void ToPlanetPOV()
+    {
+
+    }
 }
