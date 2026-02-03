3f319d90d1054dd0cb43db0022b6108efd145d44
simodewit <s.f.buikema@gmail.com>
2026-02-03 10:09:31 +0100

    Start van movement script
    De start van het creëren van de start van de virtual box voor de controller.

--- SolarSystemSimulator/Assets/Scripts/NavigationController.cs
+++ SolarSystemSimulator/Assets/Scripts/NavigationController.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class NavigationController : MonoBehaviour
 {
+    [SerializeField] private Transform _virtualBoxTarget;
+    [SerializeField] private Vector3 _virtualBoxSize;
+    
     void Start()
+    {
+        CreateVirtualBox();
+    }
+
+    private void CreateVirtualBox()
     {
         
     }
