7a4fac27ca89b7337485234ded4a1ba352d2a60a
simodewit <s.f.buikema@gmail.com>
2026-02-03 14:08:36 +0100

    Panning en start positie
    Ik heb de mogelijkheid voor een start rotatie en positie van de pivot point. Ook heb ik de panning toegevoegd, NOG NIET GETEST. WOP.

--- SolarSystemSimulator/Assets/Scripts/NavigationController.cs
+++ SolarSystemSimulator/Assets/Scripts/NavigationController.cs
@@ -1,16 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// This controller is used in the galaxy scene to move around and observe the galaxy.
 /// </summary>
 public class NavigationController : MonoBehaviour
 {
+    #region Variables
+    
+    [Header("Spawning")]
+    [Tooltip("The distance of the camera to the pivot point at startup.")]
+    [SerializeField] private float _cameraDistance = 5f;
+    [Tooltip("The position of the pivot point at startup.")]
+    [SerializeField] private Vector3 _pivotPointPosition = new(0f,0f,0f);
+    [Tooltip("The rotation of the pivot point at startup.")]
+    [SerializeField] private Vector3 _pivotPointRotation = new(0.5f,0.5f,0.5f);
+    
     [Header("Moving")]
     [Tooltip("This value can be null and will be defaulted to the transform this script is attached to. This is the point the camera will rotate around that will be moved around.")]
     [SerializeField] private Transform _pivotPoint;
-    [Tooltip("The speed at which the controller will be moving.")]
-    [SerializeField] private float _speed;
+    [Tooltip("The transform of the camera attached to the pivot point.")]
+    [SerializeField] private Transform _camera;
+    [Tooltip("The speed at which the controller moves.")]
+    [SerializeField] private float _moveSpeed = 5f;
+    [Tooltip("The speed at which the controller rotates.")]
+    [SerializeField] private float _rotateSpeed = 5f;
+    [Tooltip("The speed at which the controller scrolls.")]
+    [SerializeField] private float _scrollSpeed = 5f;
     
     [Header("Virtual box")]
     [Tooltip("The target place where the virtual box for out of bounds regulation will be created.")]
@@ -23,6 +40,13 @@ public class NavigationController : MonoBehaviour
     private Bounds _virtualBox;
     private bool _enableMovement;
     
+    #endregion
+    
+    #region Unity Callbacks
+    
+    /// <summary>
+    /// Calls at startup.
+    /// </summary>
     private void Start()
     {
         CreateVirtualBox();
@@ -31,16 +55,50 @@ public class NavigationController : MonoBehaviour
         {
             _pivotPoint = transform;
         }
+
+        SetCameraOffset();
+        SetPivotPointOffset();
     }
 
+    /// <summary>
+    /// Updates every frame.
+    /// </summary>
     private void Update()
     {
-        Pan();
-        Rotate();
-        Zoom();
+        if (_enableMovement)
+        {
+            Pan();
+            Rotate();
+            Zoom();
+        }
         
         StayInBounds(); // Call this at the end to ensure the controller is clamped AFTER moving. (better experience)
     }
+    
+    #endregion
+
+    #region Spawning
+
+    /// <summary>
+    /// Set the distance of the camera from the pivot point when spawning.
+    /// </summary>
+    private void SetCameraOffset()
+    {
+        _camera.position = new Vector3(0,0,_cameraDistance);
+    }
+
+    /// <summary>
+    /// Set the position and rotation offset of the pivot point when spawning.
+    /// </summary>
+    private void SetPivotPointOffset()
+    {
+        _pivotPoint.position = _pivotPointPosition;
+        _pivotPoint.eulerAngles = _pivotPointRotation;
+    }
+
+    #endregion
+    
+    #region Virtual box
 
     /// <summary>
     /// Sets the variables of the minimum and maximum bounds of the virtual box that will keep the controller within bounds.
@@ -78,33 +136,54 @@ public class NavigationController : MonoBehaviour
 
         _pivotPoint.position = position;
     }
+    
+    #endregion
+    
+    #region Movement
 
+    /// <summary>
+    /// Toggles the movement of the controller on or off.
+    /// </summary>
+    /// <param name="value"></param>
     public void ToggleMovement(bool value)
     {
         _enableMovement = value;
     }
     
+    /// <summary>
+    /// Moves the camera through the scene.
+    /// </summary>
     private void Pan()
     {
-        if (!_enableMovement)
+        var clickPerformed = InputManager.Instance.input.Mouse.LeftButton.IsPressed();
+
+        if (!clickPerformed)
         {
             return;
         }
+        
+        var mouseDelta = InputManager.Instance.input.Mouse.Delta.ReadValue<Vector2>();
+        var speedCalculation = mouseDelta * _moveSpeed;
+        var movement = new Vector3(speedCalculation.x, speedCalculation.y, 0f);
+
+        _pivotPoint.position += movement;
     }
 
+    /// <summary>
+    /// Rotates the camera around the pivot point.
+    /// </summary>
     private void Rotate()
     {
-        if (!_enableMovement)
-        {
-            return;
-        }
+        var mouseDelta = InputManager.Instance.input.Mouse.Delta.ReadValue<Vector2>();
     }
 
+    /// <summary>
+    /// Zooms the camera towards and away from the pivot point.
+    /// </summary>
     private void Zoom()
     {
-        if (!_enableMovement)
-        {
-            return;
-        }
+        var mouseDelta = InputManager.Instance.input.Mouse.Delta.ReadValue<Vector2>();
     }
+    
+    #endregion
 }
